2d953733584d0d17b648fe25a52e8583fb91fd29
David van der Spek <vanderspek.david@gmail.com>
2023-06-23 11:27:28 +0200

    ci: add workflow to publish packages
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,8 +2,5 @@ module.exports = {
   extends: [
     "@commitlint/config-conventional",
     "@commitlint/config-lerna-scopes"
-  ],
-  rules: {
-    "subject-case": [2, "always", "sentence-case"]
-  }
+  ]
 };
